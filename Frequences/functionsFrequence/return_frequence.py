ccf8f980be72ed4f0ddd1adf3cf46711565120d6
Otávio Sanches Bissoli Zaia <EV0064@vereda.local>
2019-02-18 14:01:41 -0300

    commit coments

--- Frequences/functionsFrequence/return_frequence.py
+++ Frequences/functionsFrequence/return_frequence.py
@@ -93,42 +93,60 @@ def getFrequenceRa(ra):
 #RETORNA FREQUENCIAS POR PERÍODO PARA UM DETERMINADO ALUNO ESPECIFICANDO A MATÉRIA QUE SE DESEJA OBTER OS DADOS
 def return_frequences_for_periods(ra):
     
+    #capturamos todos os dados do aluno informado na função
     aluno = returnRa(ra)
 
+    #dentre essas informações capturamos seu ano de ensino
     year = aluno.year
 
+    #guardará dados no formato ex.: {'Matemática':{'1ºTrimestre':4,'2ºTrimestre/1':5,'2ºTrimestre/2':8,'3ºTrimestre':0}}
+    #ou seja, a disciplina e as faltas do aluno por período na disciplina
     dic_discipline = {}
 
+    #capturamos todas as disciplinas que o aluno está cadastrado (disciplinas de seu ano de ensino)
     disciplines = getDisciplinesGroup(year)
 
+    #para cada disciplina no ano de ensino do aluno:
     for disciplineDatas in disciplines:
 
+        #pegamos o nome da disciplina
         discipline = disciplineDatas['name']
 
+        #pegamos todos os períodos letivos da escola
         periods = select_periods_all()
 
+        #esse dict guardará os períodos e as faltas dentro de cada período na disciplina em questão
         dic_periods = {}
 
+        #para cada período da escola
         for period in periods:
 
+            #este será o contador de faltas
             cont = 0
 
+            #capturamos o nome do período e quando ele começa e termina (datetime)
             period_name = period['name_periodo']
             init = period['init']
             finish = period['finish']
 
+            #vamos procurar todas as ausencias daquele aluno naquela disciplina
             query_ausences = Ausences.query.filter_by(ra = ra, discipline = discipline).all()
 
 
-
+            #para cada ausencia encontrada:
             for ausence in query_ausences:
 
+                #se a ausencia estiver dentro do intervalo de datas em que aquele período existe
+                #o contador de faltas receberá +1
                 if ausence.date >= init and ausence.date  <= finish:
 
                     cont = cont+1
 
+            #dicionário dos períodos recebe o nome do período e quantidade de faltas nele pela disciplina em questão
             dic_periods[period_name] = cont
 
+        #o dicionário da disciplina recebe as informações sobre faltas nos períodos e coloca a chave como sendo
+        #o nome da disciplina, ou seja, atribui as informações a uma determinada disciplina
         dic_discipline[discipline] = dic_periods
 
     return dic_discipline
