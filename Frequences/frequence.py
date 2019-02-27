8e9f7e37b77cbd5942a4eda28bba1d84cff8af39
Otávio Sanches Bissoli Zaia <EV0064@vereda.local>
2019-02-27 09:39:11 -0300

    azure

--- Frequences/frequence.py
+++ Frequences/frequence.py
@@ -514,11 +514,4 @@ def insertQntClasess():
 
     return 'successfull' 
 
-<<<<<<< HEAD
-=======
 
-@app.route('/test',methods=['GET'])
-def return_test():
-
-    return render_template('test-nav.html')
->>>>>>> thirty
